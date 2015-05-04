cb42c1c96fa85cb93f606fbc67c4c67840ee94f5
Dae-Won Kim <dwkim78@gmail.com>
2015-05-04 22:58:47 +0200

    non-installation of pyFFTW warning added.

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     license='MIT',
     author='Dae-Won Kim',
     author_email='dwkim78@gmail.com',
-    install_requires=['matplotlib>=1.4.3', 'numpy>=1.9.2', 'pyFFTW>=0.9.2',
-                      'scikit-learn>=0.16.1', 'scipy>=0.15.1']
+    install_requires=['matplotlib>=1.4.3', 'numpy>=1.9.2',
+        'scikit-learn>=0.16.1', 'scipy>=0.15.1']
 )
--- upsilon/__init__.py
+++ upsilon/__init__.py
@@ -1,5 +1,15 @@
 __author__ = 'kim'
 
-from upsilon import utils
+try:
+    import pyfftw
 
+    print '-------------------'
+    print '| pyFFTW detected |'
+    print '-------------------'
+except:
+    print '----------------------------------------------------------'
+    print '** WARNING: No pyFFTW detected. Numpy FFTW will be used **'
+    print '----------------------------------------------------------'
+
+from upsilon import utils
 from .extract_features.extract_features import ExtractFeatures
