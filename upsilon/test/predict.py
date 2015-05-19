e64732c08ed49f2c20bcaa42fd0117e6fc548171
Dae-Won Kim <dwkim78@gmail.com>
2015-05-19 13:46:32 +0200

    Minor updates.

--- upsilon/test/predict.py
+++ upsilon/test/predict.py
@@ -40,7 +40,7 @@ def run():
         end = time.time()
         logger.info('Feature extracting time: %.4f seconds' % (end - start))
 
-    logger.info('Extracted features:')
+    logger.info('Extracted features (17 of these are used to predict a class):')
     features = e_features.get_features_all()
     for key, value in features.iteritems():
         logger.info('   %s: %f' % (key, value))
