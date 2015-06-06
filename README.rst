ae1ca1832d2d83f47885fbad7b948a3b5e3a5028
Dae-Won Kim <dwkim78@gmail.com>
2015-06-07 00:02:05 +0200

    setup.py updated.
    for PyPi description. README.rst also added.

---
+++ README.rst
@@ -0,0 +1,10 @@
+UPSILoN (AUtomated Classification of Periodic Variable Stars using MachIne LearNing)
+aims to classify periodic variable stars such as Delta Scuti stars,
+RR Lyraes, Cepheids, Type II Cepheids, eclipsing binaries, and
+long-period variables (i.e. superclasses), and their subclasses
+(e.g. RR Lyrae ab, c, d, and e types) using
+well-sampled optical-band light curves regardless of
+survey-specific characteristics such as color, magnitude, sampling rate,
+etc (Kim+ 2015 in preparation).
+
+Visit https://github.com/dwkim78/upsilon for details.
--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import find_packages, setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(
     name='upsilon',
     version='1.1.0',
     description='Automated Classification of Periodic Variable Stars Using Machine Learning',
+    long_description=readme(),
     platforms=['any'],
     packages=find_packages(),
     include_package_data=True,
@@ -13,5 +18,15 @@ setup(
     author_email='dwkim78@gmail.com',
     install_requires=['numpy>=1.9', 'scikit-learn>=0.16.1', 'scipy>=0.15',
         #'pyfftw>=0.9.2',
-        ]
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Astronomy'
+    ]
 )
