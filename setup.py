b98c691748226f2bd0eed417db2aef230af13fee
Dae-Won Kim <dwkim78@gmail.com>
2015-10-03 14:47:28 +0800

    setup.py updated
    version update to 1.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='upsilon',
-    version='1.2.0',
+    version='1.2.1',
     description='Automated Classification of Periodic Variable Stars Using Machine Learning',
     long_description=readme(),
     platforms=['any'],
